ef5efdcca82f3358248dc0f561d13d8767c24fed
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-09-03 04:31:38 +0000

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/edit'
+  get 'groups/show'
+  get 'groups/member'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/confirm_withdraw'
+  get 'post_hobbies/new'
+  get 'post_hobbies/index'
+  get 'post_hobbies/show'
+  get 'post_hobbies/edit'
+  get 'post_hobbies/drafts'
+  get 'post_hobbies/favorites'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
