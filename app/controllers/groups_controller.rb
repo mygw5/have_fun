82dac94d2b4b7627ffd7689c2973fdd1fa13136c
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-09-09 12:13:06 +0000

    Add group-controller,index,search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,37 @@
 class GroupsController < ApplicationController
   def index
+    @q = Group.ransack(params[:q])
+    @group = Group.result(distinct: true)
   end
 
   def new
+    @group = Group.new
+  end
+
+  def create
+
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def member
   end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:group_name, :introduction, :group_image)
+  end
+
+
 end
