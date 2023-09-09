4c364c2c031ce290e52d5417dd8d6c3b7ec3e213
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-09-09 00:42:14 +0000

    Add hobbies-new/draft

--- app/controllers/post_hobbies_controller.rb
+++ app/controllers/post_hobbies_controller.rb
@@ -5,24 +5,36 @@ class PostHobbiesController < ApplicationController
   def new
     @post_hobby = PostHobby.new
     @tag_list = @post_hobby.tags.pluck(:tag_name).join(',')
+    @isDraft = @post_hobby.draft?
   end
 
   def create
-    @post_hobby = PostHobby.create(post_hobby_params.merge(user_id: current_user.id))
-    tag_list = params[:post_hobby][:tag_name].split(',')
-    if tag_list == []
-      tag = Tag.new()
-      tag.tag_name = ""
-      tag.save
-      flash.now[:alert] = "タグを入力してください"
-      render :new
-    elsif @post_hobby.save
-      @post_hobby.save_tags(tag_list)
-      flash[:notice] = "投稿に成功しました"
-      redirect_to post_hobby_path(@post_hobby)
+    if params[:commit] == "下書き保存"
+      @post_hobby = PostHobby.create(post_hobby_params.merge(user_id: current_user.id))
+      if @post_hobby.save_draft
+        flash[:notice] = "下書き保存に成功しました"
+        redirect_to drafts_post_hobbies_path
+      else
+        flash.now[:alert] = "下書き保存に失敗しました"
+        render :new
+      end
     else
-      flash.now[:alert] = "投稿に失敗しました"
-      render :new
+      @post_hobby = PostHobby.create(post_hobby_params.merge(user_id: current_user.id))
+      tag_list = params[:post_hobby][:tag_name].split(',')
+      if tag_list == []
+        tag = Tag.new()
+        tag.tag_name = ""
+        tag.save
+        flash.now[:alert] = "タグを入力してください"
+        render :new
+      elsif @post_hobby.save
+        @post_hobby.save_tags(tag_list)
+        flash[:notice] = "投稿に成功しました"
+        redirect_to post_hobby_path(@post_hobby)
+      else
+        flash.now[:alert] = "投稿に失敗しました"
+        render :new
+      end
     end
   end
 
