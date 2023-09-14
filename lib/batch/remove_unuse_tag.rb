66778453a937d2857f5de22cf2c093e65f41657e
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-09-14 15:42:57 +0000

    Add favicon

--- lib/batch/remove_unuse_tag.rb
+++ lib/batch/remove_unuse_tag.rb
@@ -1,6 +1,6 @@
 class Batch::RemoveUnuseTag
   def self.remove_unuse_tag
-    # 付属する投稿がないタグを抽出
+    # 投稿と結びつきがないタグを抽出
     tag_count_hash=Tag.joins(:post_hobbies).group(:id).count
     Tag.all.each do |tag|
       unless tag_count_hash[tag.id]
