eef0611f0be7699798f8d1cfffea4fa3969525fc
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-09-25 04:29:36 +0000

    Fix group-user-model

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -3,7 +3,7 @@ class GroupUser < ApplicationRecord
   belongs_to :user
 
   def create_notification_by(current_user)
-    notification = current_user.active_notifications.new(group_id: group.id, visited_id: owner_id, action: "participation")
+    notification = current_user.active_notifications.new(group_id: group.id, visited_id: group.owner_id, action: "participation")
     notification.save if notification.valid?
   end
 end
