3ff3fd18dfa63dfddf0a7aaae1e55155e9fe91e1
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-10-08 06:24:50 +0000

    Fix chat-controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,14 +5,9 @@ class ChatsController < ApplicationController
     @group = Group.find(params[:group_id])
     @chat = current_user.chats.new(chat_params)
     @chat.group_id = @group.id
-    if @chat.save
-      @group.create_notification_by(current_user, @chat.id)
-      flash[:notice] = "チャットを送信に成功しました"
-      redirect_to request.referer
-    else
-      flash.now[:alert] = "チャットを送信に失敗しました"
-      redirect_to :show
-    end
+    @chat.save
+
+    @group.create_notification_by(current_user, @chat.id)
   end
 
   private
