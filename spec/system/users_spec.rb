3e6dcf81d1fbf5b8b627a2dabd87aecb627761de
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-10-12 07:38:45 +0000

    Add user

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe 'ユーザログイン前のテスト' do
+  describe 'トップ画面のテスト' do
+    before do
+      visit root_path
+    end
+    
+    context '表示内容の確認'
+    
+    
+    
+  end
+
+end
